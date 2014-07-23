cb6305f1db8c9280a3bfe85713fbe578ebdc0cc8
jeffreybiles <bilesjeffrey@gmail.com>
2014-07-22 22:40:51 -0500

    each loop

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 App = Ember.Application.create()
 
-// Think of this like a rails router
 App.Router.map(function(){
-  // this creates the monsters route
   this.resource('monsters');
+});
+
+// MonstersRoute is an Ember Route
+// Ember.Route has lots of defaults, but you can overwrite any of them
+// Here we're overwriting model
+App.MonstersRoute = Ember.Route.extend({
+  // this model will be used in the monsters template
+  model: function(){
+    return ['Sparkachu', 'Charember', 'Blastasaur'];
+  }
 });
