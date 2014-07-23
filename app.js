85586f03b9c7cb4be861d57772c1c2d3f3b4c524
jeffreybiles <bilesjeffrey@gmail.com>
2014-07-23 02:21:18 -0500

    pictures

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ App.Router.map(function(){
 App.MonstersRoute = Ember.Route.extend({
   // model is now an array of object literals
   model: function(){
-    return [{name:'Sparkachu', level:3}, {name: 'Charember', level: 5}, {name: 'Blastasaur', level: 28}];
+    return [
+      {name:'Sparkachu', level:3, imageUrl: 'https://s3.amazonaws.com/monsterbattle/electric.png'},
+      {name: 'Charember', level: 5, imageUrl: 'https://s3.amazonaws.com/monsterbattle/fire.png'},
+      {name: 'Rhauk', level: 28, imageUrl: 'https://s3.amazonaws.com/monsterbattle/earth.png'}
+    ];
   }
 });
