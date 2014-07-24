aac49857538c7d88a9df7c99005ce8cdbfe79816
jeffreybiles <bilesjeffrey@gmail.com>
2014-07-24 11:59:57 -0500

    master detail view

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
 App = Ember.Application.create()
 
-// Choose the adapter you will use
-// This adapter uses fixtures
 App.ApplicationAdapter = DS.FixtureAdapter.extend({})
 
 App.Router.map(function(){
-  this.resource('monsters');
+  this.resource('monsters', {path: 'monsters'}, function(){
+    // nested resource
+    // the full path will be '/monsters/:monster_id'
+    this.resource('monster', {path: ':monster_id'});
+  });
 });
 
-// Define your data class, extended from DS.Model
 App.Monster = DS.Model.extend({
-  // you must define each attribute you want to receive from fixtures or the server
-  // The only exception is id
-  // optional type coercion by defining it as a string, number, boolean, or date
   name: DS.attr('string'),
   level: DS.attr('number'),
   imageUrl: DS.attr('string')
 })
 
-// fixtures array
-// this is what used to be in the MonstersRoute model, but with an id added
 App.Monster.FIXTURES = [
   {id: 1, name:'Sparkachu', level:3, imageUrl: 'https://s3.amazonaws.com/monsterbattle/electric.png'},
   {id: 2, name: 'Charember', level: 5, imageUrl: 'https://s3.amazonaws.com/monsterbattle/fire.png'},
@@ -28,8 +24,6 @@ App.Monster.FIXTURES = [
 
 App.MonstersRoute = Ember.Route.extend({
   model: function(){
-    // the store is what keeps track of all the data
-    // the find command with one argument with return all data of that type
     return this.store.find('monster')
   }
 });
